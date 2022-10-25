ff25c575b92a407de043534c3197f29b22b586db
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-10-25 11:26:45 +0200

    Exercice manip complet

---
+++ TP_manip_DURAND/src/tp_manip_durand/Moussaka.java
@@ -0,0 +1,18 @@
+package tp_manip_durand;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Corentin DURAND
+ */
+public class Moussaka {
+    public int nbCalories;
+    public Moussaka(int nb_cal){
+        nbCalories = nb_cal;
+}
+    
+}
