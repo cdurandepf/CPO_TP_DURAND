0d12dd68bbec8c2e192300123a05a2212977e992
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-07 11:17:54 +0100

    Premier interface graphique Une erreur est présente, lorsque l'on valdie sans rentrer de nom il affiche "vous êtes ici ici", le résultat attendut etan "Veuillez rentrer votre nom et prenom"

---
+++ MaPremierInterfaceGraphique/src/mapremierinterfacegraphique/MaPremierInterfaceGraphique.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mapremierinterfacegraphique;
+
+import javax.swing.JWindow;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Corentin DURAND
+ */
+public class MaPremierInterfaceGraphique {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+}
