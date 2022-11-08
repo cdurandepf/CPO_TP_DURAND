f7afeef770465594cba89876b238dd2ccf90ed57
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-08 15:39:41 +0100

    TP4 Création de la classe Epee et Baton Création de deux objet epee(Excalibure et durandal

---
+++ TP4_DURAND_Corentin/src/tp4_durand_corentin/TP4_DURAND_Corentin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4_durand_corentin;
+
+/**
+ *
+ * @author Corentin DURAND
+ */
+public class TP4_DURAND_Corentin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Epee epee_1 = new Epee("Excalibur",7,5);
+        Epee epee_2 = new Epee("Durandal",4,7);
+        
+    }
+    
+}
