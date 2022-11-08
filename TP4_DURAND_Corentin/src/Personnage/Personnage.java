86fb03f6b02316b3372b2bdcbd131671e9c786ae
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-08 17:10:44 +0100

    TP4 Ajout d'un inventaire et de possibilité d'équiper une arme

--- TP4_DURAND_Corentin/src/Personnage/Personnage.java
+++ TP4_DURAND_Corentin/src/Personnage/Personnage.java
@@ -1,5 +1,10 @@
 package Personnage;
 
+import java.util.*;
+import Armes.Arme;
+import Armes.Baton;
+import Armes.Epee;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,12 +19,39 @@ public class Personnage {
     public Personnage(String nom_p, int niv_vie) {
         nom = nom_p;
         niveau_vie = niv_vie;
+        arme_en_main = null;
 
     }
 
-    public String nom;
+    public String nom; 
     public int niveau_vie;
+    public Arme arme_en_main;
+    public Vector<Arme> inventair = new Vector();
 
+    public boolean add_arme(Arme arme) {
+        if (inventair.size() < 6) {
+            inventair.addElement(arme);
+            return (true);
+                    
+            
+        } else {
+            return (false);
+        }
+    }
+    
+    public Arme getter_arme_en_main(){
+        return(arme_en_main);
+    }
+    
+    public void porter_arme(String nom){
+        for(int i = 0; i < inventair.size(); i++){
+            if(inventair.elementAt(i).nom.equals("nom")){
+                arme_en_main = inventair.elementAt(i);
+                System.out.println("L'arme à bien été équiper");
+            }
+        }
+    }
+    
     @Override
     public String toString() {
         return ("Nom : " + nom + "\nNiveau de vie : " + niveau_vie);
