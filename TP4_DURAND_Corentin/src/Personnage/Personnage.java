e4711adec144de95131300bc808914861c458712
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-08 17:18:46 +0100

    TP4 fin de séance, modification léger et correction

--- TP4_DURAND_Corentin/src/Personnage/Personnage.java
+++ TP4_DURAND_Corentin/src/Personnage/Personnage.java
@@ -9,7 +9,6 @@ import Armes.Epee;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Corentin DURAND
@@ -23,7 +22,7 @@ public class Personnage {
 
     }
 
-    public String nom; 
+    public String nom;
     public int niveau_vie;
     public Arme arme_en_main;
     public ArrayList<Arme> inventair = new ArrayList();
@@ -32,29 +31,34 @@ public class Personnage {
         if (inventair.size() < 6) {
             inventair.add(arme);
             return (true);
-                    
-            
+
         } else {
             return (false);
         }
     }
-    
-    public Arme getter_arme_en_main(){
-        return(arme_en_main);
+
+    public Arme getter_arme_en_main() {
+        return (arme_en_main);
     }
-    
-    public void porter_arme(String nom){
-        for(int i = 0; i < inventair.size(); i++){
-            if(inventair.get(i).nom.equals("nom")){
+
+    public void porter_arme(String nom) {
+        for (int i = 0; i < inventair.size(); i++) {
+            if (inventair.get(i).nom.equals("nom")) {
                 arme_en_main = inventair.get(i);
                 System.out.println("L'arme à bien été équiper");
             }
         }
     }
-    
+
     @Override
     public String toString() {
-        return ("Nom : " + nom + "\nNiveau de vie : " + niveau_vie);
+        if (arme_en_main == null) {
+            return ("Nom : " + nom + "\nNiveau de vie : " + niveau_vie);
+        } else {
+            return ("Nom : " + nom + "\nNiveau de vie : " + niveau_vie
+                    + "Arme équpé : " + arme_en_main);
+        }
+
     }
 
 }
